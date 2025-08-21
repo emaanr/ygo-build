ab60898cc3ed4ca9ee72193a4dbc63f3f164b9e0
Emaan Rana <87550510+emaanr@users.noreply.github.com>
2025-08-21 15:39:30 -0400

    Re-init Frontend with Angular 20

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,20 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { PageComponent } from './components/page/page.component';
-import { DecksComponent } from './pages/decks/decks.component';
-import { SimulateComponent } from './pages/simulate/simulate.component';
-import { ApiComponent } from './pages/api/api.component';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  {
-    path: '',
-    component: PageComponent,
-    children: [
-      { path: 'decks', component: DecksComponent },
-      { path: 'simulate', component: SimulateComponent },
-      { path: 'api', component: ApiComponent },
-    ]
-  }
-];
+export const routes: Routes = [];
