ecbd18901720842937050c08254e519e1ed482bf
Emaan Rana <87550510+emaanr@users.noreply.github.com>
2025-08-21 16:46:51 -0400

    Connecting Frontend to Backend and making API request from Frontend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<YgobContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors();
+
 builder.Services.AddControllers();
 
 // Add services to the container.
@@ -14,12 +16,22 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Note: Ordering is important in this part of the config.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(options => options
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(
+        "http://localhost:4200",
+        "https://localhost:4200"
+    )
+);
 
 app.MapControllers();
 
