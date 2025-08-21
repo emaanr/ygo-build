a285fcd480f2517378bd2018ba8b060349fd2c7a
Emaan Rana <87550510+emaanr@users.noreply.github.com>
2025-08-21 16:46:51 -0400

    Connecting Frontend to Backend and making API request from Frontend

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 };
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('Frontend');
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = signal('Yu-Gi-Oh! Build');
+  protected users = signal<any>([]);
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => this.users.set(response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Completed the http request'),
+    });
+  }
+
+  // Alternative method
+  // async ngOnInit() {
+  //   this.users.set(await this.getUsers());
+  // }
+  // async getUsers() {
+  //   try {
+  //     return firstValueFrom(this.http.get('https://localhost:5001/api/users'));
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }
 }
